05bc4960753bb3189374ead730039c1debd99757
cybersecurity-1 <112893543+cybersecurity-1@users.noreply.github.com>
2024-02-06 07:39:05 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,39 @@
+var  a = "00", n1=0 ,n2=0,sum=0;
+var sub1=0,sub2=0,sub3=0;
+function buttonclick(val){
+   var four=Number(document.getElementById("input1").value)
+
+    document.getElementById("input1").value+=val;
+    
+}
+function cleardisplay(){
+    var n = " ";
+    document.getElementById("input1").value=n;
+
+}
+
+function add(){
+    
+    n1=Number(document.getElementById("input1").value);
+   
+    document.getElementById("input1").value="+";  
+}
+
+function subtraction(){
+    sub1=Number(document.getElementById("input1").value);
+    document.getElementById("input1").value="-";
+}
+
+function equal(){
+    
+    n2=Number(document.getElementById("input1").value);
+    
+    sum=n1+n2;
+    sum=sub1-n2;
+    document.getElementById("input1").value=sum;
+    
+ //   sub2=Number(document.getElementById("input1").value);
+   // sub3=sub1-sub2;
+   // document.getElementById("input1").value=sub3;
+
+}
