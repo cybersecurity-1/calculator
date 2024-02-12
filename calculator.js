bca835d73b75512f8eedbbc98db49b5637afa51f
cybersecurity-1 <112893543+cybersecurity-1@users.noreply.github.com>
2024-02-12 20:38:12 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,10 +1,14 @@
-var  a = "00", n1=0 ,n2=0,sum=0;
-var sub1=0,sub2=0,sub3=0;
+var  a = "00", n1=0 ,n2=0,sum=0,sub=0;
+
+
 function buttonclick(val){
-   var four=Number(document.getElementById("input1").value)
+   var num =Number(document.getElementById("input1").value)
 
     document.getElementById("input1").value+=val;
     
+}
+function dot(point){
+    document.getElementById("input1").value+=point;
 }
 function cleardisplay(){
     var n = " ";
@@ -12,28 +16,76 @@ function cleardisplay(){
 
 }
 
+function deleten(){
+    let display = document.getElementById("input1");
+    display.value = display.value.slice(0, -1);
+    
+}
+
 function add(){
     
     n1=Number(document.getElementById("input1").value);
    
-    document.getElementById("input1").value="+";  
+    document.getElementById("input1").value=""; 
+    s = 1; 
 }
 
 function subtraction(){
-    sub1=Number(document.getElementById("input1").value);
-    document.getElementById("input1").value="-";
+    n1=Number(document.getElementById("input1").value);
+    document.getElementById("input1").value="";
+    s=2;
 }
+function multiplication(){
+    n1=Number(document.getElementById("input1").value);
+    document.getElementById("input1").value="";
+    s=3;
+}
+function divide(){
+    n1=Number(document.getElementById("input1").value);
+    document.getElementById("input1").value=""
+    s=4;
+}
+function mode(){
+    n1=Number(document.getElementById("input1").value);
+    document.getElementById("input1").value=""
+    s=5;
+}
+
 
 function equal(){
     
     n2=Number(document.getElementById("input1").value);
-    sub2=Number(document.getElementById("input1").value);
-    sum=n1+n2;
-    sub3=sub1-sub2;
-    document.getElementById("input1").value=sum;
+        if(s == 1){
+            sum = n1+n2;
+        document.getElementById("input1").value=sum;
+
+        }
+
+        else if(s == 2){
+            sub = n1-n2;
+        document.getElementById("input1").value=sub;
+
+        }
+        else if(s == 3){
+            mul = n1*n2;
+        document.getElementById("input1").value=mul;
+
+        }
+        else if(s == 4){
+            div = n1/n2;
+            document.getElementById("input1").value=div;
+        }
+       else if(s == 5){
+            div = n1%n2;
+            document.getElementById("input1").value=div;
+       }
+   
     
    
-   // sub3=sub1-sub2;
-    document.getElementById("input1").value=sub3;
+   // document.getElementById("input1").value=sum;
+
+    
+    
+ 
 
 }
